4ba03c8d692349c4d2d525b164780c322916b69d
HunBot <hunter.wu+001@gmail.com>
2017-06-05 13:09:36 +0800

    Auto commit @20170605

--- bin/cron/daily.sh
+++ bin/cron/daily.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 PATH=$PATH:/usr/local/bin
 export DISPLAY=:0
-cd /data/app/taiwan-rate-history-charts/src/crawler;
+cd /data/app/taiwan-rate-history-charts;
 git pull;
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch img/*.png' --prune-empty --tag-name-filter cat -- --all;
+mkdir img;
+cd src/crawler;
 /usr/local/bin/casperjs test main.js;
 cd ../..;
 git commit -am "Auto commit @"`date +%Y%m%d`
-git push;
+git push origin --force --all;
